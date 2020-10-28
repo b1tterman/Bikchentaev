508166fb1345a35c5d91886f0d87f5cff97c1d9a
b1tterman <b1tterman@ya.ru>
2020-10-28 14:24:00 +0300

    changed first task code

--- test1.py
+++ test1.py
@@ -1,15 +1,8 @@
 # Составить алгоритм: если введенное число больше 7, то вывести “Привет”
 
-def is_float(value):
-  try:
-    float(value)
-    return True
-  except:
-    return False
-
 print('Введите любое число')
 x = float(input())
-if is_float(x):
+if isinstance(x, (float, int)):
     if x > 7:
      print("Привет")
      input()
